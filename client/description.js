b48c6740050d38eb9373340b02ccbf2c59c1c790
Jessica Hasal <jessicahasal96@gmail.com>
2018-11-06 14:23:21 -0800

    added css, reseeded

--- client/description.js
+++ client/description.js
@@ -4,22 +4,31 @@ const Description = (props) => {
 
   return (
     <div id="description">
-      <div className="ownerSec">
-        <img className="ratingIcon" src={props.description.imageUrl}></img>
-          {props.description.Owner} Owner
+      <div id ="owner">
+        <div className="ownerSec">
+          <img className="ownerIcon" src={props.description.imageUrl}></img>
+          <div className="ownerName">{props.description.Owner}</div>
+        </div>
       </div>
       <div className="descriptionBox">
+        <div className="sizeHeader">{props.description.full}</div>
         <div className="name">{props.description.houseName}</div>
         <div className="city">{props.description.city}</div>
         <div className="size">{props.description.full}</div>
          <div className="house">{props.description.bedrooms} bedrooms {props.description.beds} beds {props.description.guests} guests {props.description.baths} baths</div>
-        <div className="description">About this house {props.description.description}</div>
-        <div className="amenities">Amenities -{props.description.amenities}</div>
-
+        <div className="description">{props.description.description}</div>
+        <div className="contact">Contact host</div>
+        <div className="amenitiesHead">Amenities </div>
+        <div className="bullets">-{props.description.amenities}</div>
+        <div className="lockHead"> Self check-in </div>
+        <div className= "lockOption"> Easily check yourself in with the {props.description.lock}
+        <div className = ""
       </div>
+
     </div>
   )
 
 }
 
 export default Description;
+
