3e5164463ef4c3c354b1944a783b4d92439e8547
Jessica Hasal <jessicahasal96@gmail.com>
2018-11-06 14:23:21 -0800

    added css, reseeded

--- database-mongodb/seed.js
+++ database-mongodb/seed.js
@@ -3,8 +3,10 @@ const Description = require('./description.js');
 const faker = require('faker');
 
 for (var i = 1; i <= 100; i++) {
-  let amenitiesArr = ["Pool", "Spa", "Laundry", "Parking", "Heating", "Wifi"];
+  let amenitiesArr = ["Pool", "Spa", "Laundry", "Parking", "Heating", "Wifi"]
+  let checkIn = ["keypad", "lockbox", "smartlock"]
   let optionsArr = ["Entire house", "Entire apartment", "Entire Townhouse"]
+  let percent = [90,91,92,93,94,95,96,97,98];
   let sampleData = {};
     sampleData.imageUrl = faker.image.avatar();
     sampleData.Owner = faker.name.findName();
@@ -15,7 +17,9 @@ for (var i = 1; i <= 100; i++) {
     sampleData.guests = Math.floor(Math.random() * 2) + 1;
     sampleData.baths = Math.floor(Math.random() * 7) + 1;
     sampleData.amenities = amenitiesArr[Math.floor(Math.random() * amenitiesArr.length)]
-    sampleData.full = optionsArr[Math.floor(Math.random() * optionsArr.length)]
+    sampleData.full = optionsArr[Math.floor(Math.random() * optionsArr.length)];
+    sampleData.lock = checkIn[Math.floor(Math.random() * checkIn.length)];
+    sampleData.lock = percent[Math.floor(Math.random() * checkIn.length)];
     //sampleData.city = faker.address.city();
 
 
